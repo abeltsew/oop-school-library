c7291ab1af4ea92f3da9267c13b73410a842fe21
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-06-16 13:33:26 +0300

    Fix Space Issue

--- student.rb
+++ student.rb
@@ -2,7 +2,7 @@ require_relative 'person'
 
 class Student < Person
   def initialize(classroom, age, parent_permission: true, name: 'Unknown')
-    super(name,age,parent_permission)
+    super(name, age, parent_permission)
     @classroom = classroom
   end
 
