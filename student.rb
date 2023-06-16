33a5595eef145a3f6f9958107cca937e3312e004
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-06-16 13:11:03 +0300

    Add Student Class

---
+++ student.rb
@@ -0,0 +1,13 @@
+require_relative 'person'
+
+class Student < Person
+    def initialize(classroom)
+        super()
+        @classroom = classroom
+    end
+
+    def play_hooky 
+        "¯\(ツ)/¯"
+    end
+    
+end
