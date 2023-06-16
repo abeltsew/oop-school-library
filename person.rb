9deea4ee8410a85c1892b677f3a755e1d441cec6
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-06-16 13:16:14 +0300

    Fix Linter

--- person.rb
+++ person.rb
@@ -1,20 +1,20 @@
-class Person 
-    def initialize(age, name = "Unknown",  parent_permission = true)
-        @id = Random.rand(1..1000)
-        @name = name
-        @age = age
-      @parent_permission = parent_permission
+class Person
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+  attr_reader :id
+  attr_accessor :name, :age
 
-    end
-    attr_reader :id 
-    attr_accessor :name, :age
+  def can_use_service?
+    of_age? || @parent_permission
+  end
 
-    private def of_age? 
-        age >= 18
-    end
+  private
 
-    def can_use_service?
-        of_age? || @parent_permission
-    end
-
+  def of_age?
+    age >= 18
+  end
 end
--- teacher.rb
+++ teacher.rb
@@ -1,12 +1,12 @@
 require_relative 'person'
 
 class Teacher < Person
-    def initialize(specialization)
-        super()
-        @specialization = specialization
-    end
+  def initialize(specialization)
+    super()
+    @specialization = specialization
+  end
 
-    def can_use_service? 
-         true
-    end
+  def can_use_service?
+    true
+  end
 end
