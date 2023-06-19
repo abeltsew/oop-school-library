23abe6ef36d2b420405a853ab5db33834059ae21
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-06-19 20:31:02 +0300

    Fix Linter

--- teacher.rb
+++ teacher.rb
@@ -2,7 +2,7 @@ require_relative 'person'
 
 class Teacher < Person
   def initialize(age, name, specialization, parent_permission: true)
-    super(age, name, parent_permission:)
+    super(age, name, parent_permission: parent_permission)
     @specialization = specialization
   end
 
