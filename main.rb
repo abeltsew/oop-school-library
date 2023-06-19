790c9407649e55914fc331c1c162831b9837702d
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-06-19 21:12:37 +0300

    Refactor main

--- main.rb
+++ main.rb
@@ -18,27 +18,31 @@ def options
   puts '7 - Exit'
 end
 
+def manage_options(app)
+  selected = Integer(gets.chomp)
+  case selected
+  when 1
+    app.list_books
+  when 2
+    app.list_people
+  when 3
+    app.create_person
+  when 4
+    app.create_book
+  when 5
+    app.create_rental
+  when 6
+    app.list_rental
+  when 7
+    exit
+  end
+end
+
 def main
   app = App.new
   loop do
     options
-    selected = Integer(gets.chomp)
-    case selected
-    when 1
-      app.list_books
-    when 2
-      app.list_people
-    when 3
-      app.create_person
-    when 4
-      app.create_book
-    when 5
-      app.create_rental
-    when 6
-      app.list_rental
-    when 7
-      exit
-    end
+    manage_options(app)
   end
 end
 
