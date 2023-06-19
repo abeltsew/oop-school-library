2dd4655608825b5d69dee79153e03b5bd28ff91e
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-06-19 21:03:55 +0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -17,37 +17,37 @@ class App
 
   def create_teacher
     print "teacher's specialization: "
-      specialization = gets.chomp
-      print "teacher's age: "
-      age = gets.chomp
-      print "teacher's name: "
-      name = gets.chomp
-      teacher = Teacher.new(specialization, age, name, parent_permission: true)
-      @people << teacher
-      puts 'You have successfully registered a Teacher'
+    specialization = gets.chomp
+    print "teacher's age: "
+    age = gets.chomp
+    print "teacher's name: "
+    name = gets.chomp
+    teacher = Teacher.new(specialization, age, name, parent_permission: true)
+    @people << teacher
+    puts 'You have successfully registered a Teacher'
   end
 
   def create_student
     print 'Age: '
-      age = Integer(gets.chomp)
-      print 'Name: '
-      name = gets.chomp
-      print 'Has parent permission? [Y/N]'
-      parent_permission = gets.chomp.downcase
+    age = Integer(gets.chomp)
+    print 'Name: '
+    name = gets.chomp
+    print 'Has parent permission? [Y/N]'
+    parent_permission = gets.chomp.downcase
 
-      case parent_permission
-      when 'n'
-        student = Student.new(nil, age, name, parent_permission: false)
-        @people << student
-      when 'y'
-        student = Student.new(nil, age, name, parent_permission: true)
-        @people << student
-      else
-        'You have entered an invalid option'
-      end
-
-      puts 'You have successfully registered a Student'
+    case parent_permission
+    when 'n'
+      student = Student.new(nil, age, name, parent_permission: false)
+      @people << student
+    when 'y'
+      student = Student.new(nil, age, name, parent_permission: true)
+      @people << student
+    else
+      'You have entered an invalid option'
     end
+
+    puts 'You have successfully registered a Student'
+  end
 
   def create_person
     print 'Do you want to create a new student (1) or teacher (2)? [Inout the number]: '
