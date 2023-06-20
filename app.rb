d1ffac1e599b33a2525e2aef5e32d9a7d4523e55
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-06-20 11:52:14 +0300

    Fix Order of Params

--- app.rb
+++ app.rb
@@ -25,7 +25,7 @@ class App
     age = gets.chomp
     print "teacher's name: "
     name = gets.chomp
-    teacher = Teacher.new(specialization, age, name, parent_permission: true)
+    teacher = Teacher.new(age, name, specialization, parent_permission: true)
     @people << teacher
     puts 'You have successfully registered a Teacher'
   end
